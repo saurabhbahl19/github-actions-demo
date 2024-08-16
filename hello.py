ba8b982f5a0602984805564d7633adc8f8a9960c
saurabhbahl19 <saurabhbahl19@gmail.com>
2024-08-16 17:08:27 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello, GitHub Actions!")
+import sys
+
+if len(sys.argv) > 1:
+    version = sys.argv[1]
+else:
+    version = "default_version"
+
+print(f"Running script with version: {version}")
